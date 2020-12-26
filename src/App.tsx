b2efbc706e910edbb127a569cc1856e85179cfd0
brunosana <brunosa@dcomp.ufs.br>
2020-12-26 19:33:13 -0300

    refactor: App component in Typescript Patter

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { AiFillAlert } from 'react-icons/ai';
 import Input from './components/Input';
 
-function App() {
-    return (
-        <>
-            <h1>Hello World</h1>
-            <Input
-                name="email"
-                icon={AiFillAlert}
-                type="text"
-                placeholder="E-mail"
-            />
-        </>
-    );
-}
+const App: React.FC = () => (
+    <>
+        <h1>Hello World</h1>
+        <Input
+            name="email"
+            icon={AiFillAlert}
+            type="text"
+            placeholder="E-mail"
+        />
+    </>
+);
 
 export default App;
