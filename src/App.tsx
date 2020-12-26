4fa5c9a3541e9c4e3dabd010ed7234b85aaf8372
brunosana <brunosa@dcomp.ufs.br>
2020-12-26 19:23:19 -0300

    feat: create Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { AiFillAlert } from 'react-icons/ai';
+import Input from './components/Input';
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Input
+                name="email"
+                icon={AiFillAlert}
+                type="text"
+                placeholder="E-mail"
+            />
+        </>
+    );
 }
 
 export default App;
