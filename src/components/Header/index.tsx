c62ad3dbf20f60eef45bcf2da4f88a3a63d2a6f3
brunosana <brunosa@dcomp.ufs.br>
2020-12-27 14:45:06 -0300

    refactor: Header responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import { AiOutlineSearch, AiOutlinePoweroff } from 'react-icons/ai';
+import React, { useState } from 'react';
+import {
+    AiOutlineSearch,
+    AiOutlinePoweroff,
+    AiOutlineBars,
+    AiOutlineCloseCircle,
+} from 'react-icons/ai';
 import { Container, Content, Search, Logout } from './styles';
 
-const Header: React.FC = () => (
-    <Container>
-        <Content>
-            <nav>
+const Header: React.FC = () => {
+    const [showSideBar, setShowSideBar] = useState(false);
+
+    function handleShowSideBar() {
+        setShowSideBar(!showSideBar);
+    }
+
+    return (
+        <Container showSideBar={showSideBar}>
+            <div id="sideBar">
                 <a href="oi">Home</a>
                 <a href="oi">Add Music</a>
-            </nav>
-            <Search>
-                <AiOutlineSearch size={35} />
-                <input type="text" placeholder="Search for any music here..." />
-            </Search>
-            <Logout>
-                <AiOutlinePoweroff />
-                logout
-            </Logout>
-        </Content>
-    </Container>
-);
+                <a href="oi">Logout</a>
+            </div>
+            <Content>
+                <nav id="navHorizontal">
+                    <a href="oi">Home</a>
+                    <a href="oi">Add Music</a>
+                </nav>
+                <button
+                    type="button"
+                    id="navVertical"
+                    onClick={handleShowSideBar}
+                >
+                    {showSideBar ? (
+                        <AiOutlineCloseCircle size={25} />
+                    ) : (
+                        <AiOutlineBars size={25} />
+                    )}
+                </button>
+                <Search>
+                    <AiOutlineSearch size={35} />
+                    <input
+                        type="text"
+                        placeholder="Search for any music here..."
+                    />
+                </Search>
+                <Logout>
+                    <AiOutlinePoweroff />
+                    <i id="logoutText">logout</i>
+                </Logout>
+            </Content>
+        </Container>
+    );
+};
 
 export default Header;
